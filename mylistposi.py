47bd0a3c02d2e95b356bb9f2ec6e3459fa5cdf04
Kiran kumar Mandal <92847355+Chiku200@users.noreply.github.com>
2022-08-26 19:38:15 +0530

    Add files via upload

---
+++ mylistposi.py
@@ -0,0 +1,21 @@
+print("input list1:")
+mylistposi1=[12,-7,5,64,-14]
+print(mylistposi1)
+
+for i in mylistposi1:
+     if i<0:
+        mylistposi1.remove(i)
+     
+print("The positive Number are:")
+print(mylistposi1)
+
+print("input list2")
+mylistposi2=[12,14,-95,3]
+print(mylistposi2)
+
+for j in mylistposi2:
+    if j<0:
+         mylistposi2.remove(j)
+
+print("The positive Number are:")         
+print(mylistposi2)         
